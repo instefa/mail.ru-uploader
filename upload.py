badd02be80f475271ed12deb5b01aaeabd1c46b7
pymancer <pymancer@gmail.com>
2016-08-11 22:43:08 +0300

    directory traversing stub

--- upload.py
+++ upload.py
@@ -55,8 +55,8 @@ IS_FROZEN = getattr(sys, 'frozen', False)
 LOGIN = config.get('Credentials', 'Email', fallback='your_email@mail.ru')
 # email password
 PASSWORD = config.get('Credentials', 'Password', fallback='your_email_password')
-# relative to cloud root, must end with slash, create this folder in the cloud before using this module
-CLOUD_PATH = config.get('Locations', 'CloudPath', fallback='backups/')
+# absolute cloud path (without 'home')
+CLOUD_PATH = config.get('Locations', 'CloudPath', fallback='/backups')
 # local folder path with files to upload, use '.' to set path relative to the module location
 UPLOAD_PATH = config.get('Locations', 'UploadPath', fallback='./upload')
 # local folder to move uploaded files, will be created if not exists
@@ -75,7 +75,7 @@ VERIFY_SSL = True # True, use False only for debug and if you know what you're d
 CLOUD_DOMAIN_ORD = 2 # 2 - practice, 1 - theory
 API_VER = 2 # 2 - constant so far
 TIME_AMEND = '0246' # '0246', exact meaning has not been quite sorted out yet
-CLOUD_CONFLICT = 'strict' # 'strict' - should remain unchanged at least until 'rename' implementation
+CLOUD_CONFLICT = 'strict' # 'strict' - should remain constant at least until 'rename' implementation
 MAX_FILE_SIZE = 2*1024*1024*1024 # 2*1024*1024*1024 (bytes ~ 2 GB), API constraint
 FILES_TO_PRESERVE = ('application/zip', ) # do not archive already zipped files
 QUOTED_LOGIN = quote_plus(LOGIN) # just for convenience
@@ -150,17 +150,20 @@ def get_cloud_space(session, csrf=''):
         return total_bytes - used_bytes
     return 0
 
-def post_file(session, domain='', filename='', filetype=''):
-    assert domain is not None, 'No upload domain provided'
-    assert filename is not None, 'No file to upload provided'
+def post_file(session, domain='', file=''):
+    """ 'file' should be filename with path """
+    assert domain is not None, 'No domain to upload provided'
+    assert file is not None, 'No file to upload provided'
 
+    filetype = guess_type(file)[0]
     if not filetype:
-        logger.warning('No file type provided, using default: {}'.format(DEFAULT_FILETYPE))
+        logger.warning('File {} type unknown, using default: {}'.format(file, DEFAULT_FILETYPE))
         filetype = DEFAULT_FILETYPE
 
+    filename = os.path.basename(file)
     timestamp = str(int(time.mktime(datetime.datetime.now().timetuple()))) + TIME_AMEND
     url = urljoin(domain, '?cloud_domain=' + str(CLOUD_DOMAIN_ORD) + '&x-email=' + QUOTED_LOGIN + '&fileapi' + timestamp)
-    m = MultipartEncoder(fields={'file': (quote_plus(filename), open(os.path.join(UPLOAD_PATH, filename), 'rb'), filetype)})
+    m = MultipartEncoder(fields={'file': (quote_plus(filename), open(file, 'rb'), filetype)})
 
     r = session.post(url, data=m, headers={'Content-Type': m.content_type}, verify = VERIFY_SSL)
     if r.status_code == requests.codes.ok:
@@ -169,68 +172,102 @@ def post_file(session, domain='', filename='', filetype=''):
             size = int(r.content[41:-2])
             return (hash, size)
         else:
-            logger.error('File {} post error, no hash and size obtained'.format(filename))
+            logger.error('File {} post error, no hash and size obtained'.format(file))
     else:
-        logger.error('File {} post error, http code: {}, msg: {}'.format(filename, r.status_code, r.text))
+        logger.error('File {} post error, http code: {}, msg: {}'.format(file, r.status_code, r.text))
     return (None, None)
 
 
-def add_file(session, filename='', hash='', size=0, csrf=''):
-    assert filename is not None, 'No file to upload passed'
-    assert hash is not None, 'No hash passed'
-    assert size is not None, 'No size passed'
+def add_file(session, file='', hash='', size=0, csrf=''):
+    """ 'file' should be filename with absolute cloud path """
+    assert file is not None, 'No file to upload passed'
+    assert path is not None, 'No cloud path to upload passed'
+    assert len(hash) == 40, 'No hash passed'
+    assert size > 0, 'No size passed'
     assert csrf is not None, 'No CSRF token passed'
 
     url = urljoin(CLOUD_URL, 'file/add')
     # api, email, x-email, x-page-id (not implemented), build (not implemented) - optional parameters
-    postdata = {'home': CLOUD_PATH + filename, 'hash': hash, 'size': size, 'conflict': CLOUD_CONFLICT, 'token': csrf,
+    postdata = {'home': file, 'hash': hash, 'size': size, 'conflict': CLOUD_CONFLICT, 'token': csrf,
                 'api': API_VER, 'email':LOGIN, 'x-email': LOGIN}
 
     r = s.post(url, data=postdata, headers={'Content-Type': 'application/x-www-form-urlencoded'}, verify=VERIFY_SSL)
     if r.status_code == requests.codes.ok:
         return True
     else:
-        logger.error('File {} addition error, http code: {}, msg: {}'.format(filename, r.status_code, r.text))
+        logger.error('File {} addition error, http code: {}, msg: {}'.format(file, r.status_code, r.text))
+    return None
+
+
+def create_folder(session, folder='', csrf=''):
+    """ Takes 'folder' as new folder name with full cloud path (without 'home'),
+    returns True even if target folder already existed
+    Path should start with forward slash,
+    no final slash should be present after the target folder name
+    """
+    assert folder is not None, 'No folder path provided'
+    assert csrf is not None, 'No CSRF provided'
+    url = urljoin(CLOUD_URL, 'folder/add')
+    # api, email, x-email, x-page-id (not implemented), build (not implemented) - optional parameters
+    postdata = {'home': folder, 'conflict': CLOUD_CONFLICT, 'token': csrf,
+                'api': API_VER, 'email':LOGIN, 'x-email': LOGIN}
+    r = s.post(url, data=postdata, headers={'Content-Type': 'application/x-www-form-urlencoded'}, verify=VERIFY_SSL)
+    if r.status_code == requests.codes.ok:
+        return True
+    elif r.status_code == requests.codes.bad:
+        try:
+            r_error = r.json()['body']['home']['error']
+        except KeyError:
+            r_error = None
+        if r_error == 'exists':
+            logger.warning('Folder {} already exists'.format(folder))
+            return True
+        else:
+            logger.error('Folder {} creating failed, http code 400. Error: {}'.format(r_error))
+    else:
+        logger.error('Folder {} creating error, http code: {}, msg: {}'.format(folder, r.status_code, r.text))
     return None
 
 
 def zip_file(file):
     """ creates compressed zip files with same name and 'zip' extension
     on success deletes original file
-    on failure returns original file name
+    on failure returns original file
     replaces existing archives
     """
-    file_root, file_ext = os.path.splitext(file)
+    file_path, file_name = os.path.split(file)
+    file_root, file_ext = os.path.splitext(file_name)
     zip_name = file_root + '.zip'
     compression = zipfile.ZIP_DEFLATED
     try:
-        zf = zipfile.ZipFile(os.path.join(UPLOAD_PATH, zip_name), mode='w')
+        zf = zipfile.ZipFile(os.path.join(file_path, zip_name), mode='w')
         zf.debug = 0
         # convert unicode file names to byte strings if any
-        zf.write(os.path.join(UPLOAD_PATH, file), arcname=file, compress_type=compression)
+        zf.write(file, arcname=file_name, compress_type=compression)
     except Exception as e:
         logger.error('Failed to archive {}, error:'.format(file, e))
-        zip_name = file
+        zip_name = file_name
     else:
-        os.unlink(os.path.join(UPLOAD_PATH, file))
+        os.unlink(file)
     finally:
         zf.close()
-    return zip_name
+    return os.path.join(file_path, zip_name)
 
 
 def get_dir_files(path=UPLOAD_PATH, space=0):
     """ returns list of the cwd files, follows cloud restrictions """
     assert space is not None, 'No cloud space left or space fetching error'
 
-    for file in next(os.walk(UPLOAD_PATH))[2]:
+    for filename in next(os.walk(path))[2]:
+        file = os.path.join(path, filename)
         # in case we uploading current directory
-        if file in FILES_TO_SKIP and UPLOAD_PATH == '.':
+        if filename in FILES_TO_SKIP and path == '.':
             continue
         # in case some files are already zipped
         if ARCHIVE_FILES and guess_type(file)[0] not in FILES_TO_PRESERVE:
-            file = zip_file(file)
+            full_filename = zip_file(file)
         # api restriction
-        file_size = os.path.getsize(os.path.join(path,file))
+        file_size = os.path.getsize(file)
         if file_size < MAX_FILE_SIZE:
             if file_size < space:
                 yield file
@@ -249,8 +286,20 @@ def get_yes_no(value):
     return 'yes' if value else 'no'
 
 
+def create_cloud_path(cloud_base, local_base, path):
+    """ example:
+    cloud_base='/backups'
+    local_base='./upload'
+    path='./upload\\level1_1'
+    result='/backups/level1_1'
+    """
+    normalized_path = path.replace('\\', '/')
+    clean_path = normalized_path.replace(local_base, '', 1)
+    return cloud_base + clean_path
+
+
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """ Get absolute path to resource (source or frozen) """
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
 
@@ -279,26 +328,33 @@ if __name__ == '__main__':
             FILES_TO_SKIP.add(self_file)
     assert os.path.isfile(cacert), 'Fatal Error. CA certificate not found.'
     os.environ["REQUESTS_CA_BUNDLE"] = cacert
+    # some day this conditional mess should be replaced with class
     # cloud credentials should be in the configuration file
     if IS_CONFIG_PRESENT:
-        # uploading files
+        # preparing to upload
         uploaded_files = set()
         with requests.Session() as s:
             cloud_csrf = get_cloud_csrf(s)
             if cloud_csrf:
                 upload_domain = get_upload_domain(s, csrf=cloud_csrf)
                 if upload_domain and os.path.isdir(UPLOAD_PATH):
-                    for file in get_dir_files(space=get_cloud_space(s, csrf=cloud_csrf)):
-                        hash, size = post_file(s, domain=upload_domain, filename=file, filetype=guess_type(file)[0])
-                        if size and hash:
-                            logger.info('File {} successfully posted'.format(file))
-                            if add_file(s, filename=file, hash=hash, size=size, csrf=cloud_csrf):
-                                logger.info('File {} successfully added'.format(file))
-                                uploaded_files.add(file)
+                    for folder in os.walk(UPLOAD_PATH)[0]:
+                        # cloud dir should exist before uploading
+                        cloud_path = create_cloud_path(UPLOAD_PATH, folder)
+                        create_folder(s, folder=cloud_path, csrf=cloud_csrf)
+                        # uploading files
+                        for file in get_dir_files(path=folder, space=get_cloud_space(s, csrf=cloud_csrf)):
+                            hash, size = post_file(s, domain=upload_domain, file=file)
+                            if size and hash:
+                                logger.info('File {} successfully posted'.format(file))
+                                cloud_file = cloud_path + '/' + os.path.basename(file)
+                                if add_file(s, file=cloudfile, hash=hash, size=size, csrf=cloud_csrf):
+                                    logger.info('File {} successfully added'.format(file))
+                                    uploaded_files.add(file)
+                                else:
+                                    logger.error('File {} addition failed'.format(file))
                             else:
-                                logger.error('File {} addition failed'.format(file))
-                        else:
-                            logger.error('File {} post failed'.format(file))
+                                logger.error('File {} post failed'.format(file))
                 else:
                     logger.error('Upload failed, check settings in <{}>'.format(CONFIG_FILE))
             else:
@@ -306,19 +362,25 @@ if __name__ == '__main__':
         uploaded_num = len(uploaded_files)
         logger.info('{} file(s) successfully uploaded'.format(uploaded_num))
         if MOVE_UPLOADED:
-            if not os.path.exists(UPLOADED_PATH):
-                os.makedirs(UPLOADED_PATH)
+            upload_dir = os.path.abspath(UPLOAD_PATH)
+            uploaded_dir = os.path.abspath(UPLOADED_PATH)
             for file in uploaded_files:
-                os.rename(os.path.join(UPLOAD_PATH, file), os.path.join(UPLOADED_PATH, file))
+                file_dir, file_name  = os.path.split(os.path.abspath(file))
+                # pretty unreliable way to create path
+                file_new_dir = file_dir.replace(upload_dir, uploaded_dir, 1)
+                os.makedirs(file_new_dir, exist_ok=True)
+                os.rename(file, os.path.join(file_new_dir, file_name))
         elif REMOVE_UPLOADED and uploaded_files:
             for file in uploaded_files:
-                os.unlink(os.path.join(UPLOAD_PATH, file))
+                os.unlink(file)
         print('{} file(s) uploaded. See {} for details.'.format(uploaded_num, LOG_PATH))
     else:
         # creating a default config if local configuration does not exists
         config['Credentials'] = {'Email': LOGIN, 'Password': PASSWORD}
         config['Locations'] = {'CloudPath': CLOUD_PATH, 'UploadPath': UPLOAD_PATH, 'UploadedPath': UPLOADED_PATH}
-        config['Behaviour'] = {'ArchiveFiles': get_yes_no(ARCHIVE_FILES), 'RemoveUploaded': get_yes_no(REMOVE_UPLOADED)}
+        config['Behaviour'] = {'ArchiveFiles': get_yes_no(ARCHIVE_FILES),
+                               'MoveUploaded': get_yes_no(MOVE_UPLOADED),
+                               'RemoveUploaded': get_yes_no(REMOVE_UPLOADED)}
         with open(CONFIG_FILE, mode='w') as f:
             config.write(f)
         logger.warning('No configuration file () provided. Prepare it and run module again.'.format(CONFIG_FILE))
